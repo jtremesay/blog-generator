94fb6874a14ea3820cc0ee7de26a7e5cde167861
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2020-09-13 20:21:26 +0200

    parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        return Err(String::from("usage: blog-generator config_file output_dir"));
+    }
+
+    let config_file_path = &args[1];
+    let output_path = &args[2];
+
+    Ok(())
 }
